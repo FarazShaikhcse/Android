f550f9211a6b7f93b2f7e6afdf39e0715f8c4b54
Faraz Shabbir Shaikh <55327028+FarazShaikhcse@users.noreply.github.com>
2020-02-28 12:40:44 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,63 +1,45 @@
-package com.cs037.progressfar;
+package com.cs037.music;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.os.AsyncTask;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.TextView;
+import android.widget.RelativeLayout;
 
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
+import java.util.Random;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    ProgressBar pb;
-    Button b1;
-    TextView t1;
+public class MainActivity extends AppCompatActivity  {
+Button b1,b2,b3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        pb=(ProgressBar)findViewById(R.id.progressBar2);
         b1=(Button)findViewById(R.id.button);
-        t1=(TextView)findViewById(R.id.textView);
-        b1.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-        pb.setProgress(0);
-        T t1=new T();
-        t1.execute("100");
-    }
-    class T extends AsyncTask<String,Integer,String>
-    {
-        int i=0;
-
-        @Override
-        protected String doInBackground(String... strings) {
-            int max=Integer.parseInt(strings[0]);
-            while(i<max)
-            {
-                try{
-                    Thread.sleep(100);
-                    i++;
-                    publishProgress(i);
-                }
-                catch(Exception e)
-                {
-
-                }
+        b2=(Button)findViewById(R.id.button2);
+        b3=(Button)findViewById(R.id.button3);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(getApplicationContext(),MyService.class));
             }
-            return null;
-        }
-
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-            pb.setProgress(values[0]);
-            super.onProgressUpdate(values);
-            t1.setText(values[0].toString());
-        }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(getApplicationContext(),MyService.class));
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Main2Activity.class));
+            }
+        });
     }
+
+
 }
